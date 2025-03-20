f85d9d236f4488bb6e12f4e72cf17e0db06cdcb8
mecaneer23 <mecaneer23@gmail.com>
2025-03-20 16:14:38 +0200

    feat: allow better reloading of calculator

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
-# pyright: ignore
-# type: ignore
+"""Brython html UI for calculate"""
+# type: ignore[reportOperatorIssue, reportUnusedExpression]
+# ruff: noqa: B015
 
 from browser import DOMEvent, bind, document, html
 from calculate import get_packages
@@ -26,26 +27,24 @@ form <= submit
 
 document <= form
 
-wrapper_defined = False
-
-# rides = int(input("Amount of rides: "))
-# ride_len = int(input("Minutes per ride: "))
+wrapper = html.DIV()
+wrapper.id = "wrapper"
+document <= wrapper
 
 
 @bind(submit, "click")
 def on_submit(event: DOMEvent) -> None:
+    """Handle submit button press"""
     event.preventDefault()
-    global wrapper_defined
-    if wrapper_defined:
-        del document["wrapper"]
-    wrapper_defined = True
     rides = int(rides_input.value)
     ride_len = int(len_input.value)
     dott = get_packages()
 
-    wrapper = html.DIV()
+    wrapper = document["wrapper"]
+    wrapper.clear()
     wrapper <= html.P(
-        f"Best package for given price: {min(dott, key=lambda p: p.get_price(rides, ride_len))}"
+        f"Best package for given price: "
+        f"{min(dott, key=lambda p: p.get_price(rides, ride_len))}",
     )
     wrapper <= html.HR()
     wrapper <= html.P("Cost for all packages:")
@@ -53,7 +52,7 @@ def on_submit(event: DOMEvent) -> None:
     table <= html.TR([html.TD("Package price"), html.TD("Total cost")])
     for p in dott:
         table <= html.TR(
-            [html.TD(str(p)), html.TD(p.get_price(rides, ride_len))]
+            [html.TD(str(p)), html.TD(p.get_price(rides, ride_len))],
         )
     wrapper <= table
     document <= wrapper
